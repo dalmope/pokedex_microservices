ec24dbc054b74821e32327d41f8fc06abc2777f9
dalmope <eukadaju@gmail.com>
2022-08-31 19:58:33 -0500

    config server

---
+++ config-server/src/test/java/com/dalmope/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalmope.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
